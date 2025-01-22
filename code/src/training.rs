466effc86317762195c81ab9b8cd567fb37af748
Kamil Pomykała <kamil@kamilpomykala.dev>
2025-01-22 16:44:10 +0100

    feat: Add configuration saving functionality during training
    - Introduced functionality to save training configuration to a JSON file.
    - The configuration is saved in the specified artifact directory.
    - This ensures reproducibility by preserving the training parameters used.

--- code/src/training.rs
+++ code/src/training.rs
@@ -27,6 +27,9 @@ pub struct TrainingConfig {
 
 pub fn train<B: AutodiffBackend>(artifact_dir: &str, config: TrainingConfig, device: B::Device) {
     create_artifact_dir(artifact_dir);
+    config
+        .save(format!("{artifact_dir}/config.json"))
+        .expect("Config should be saved successfully");
 
     B::seed(config.seed);
 
