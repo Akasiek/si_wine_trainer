f259fbc52950357133d78acd8438412de523bbb6
Kamil Pomykała <kamil@kamilpomykala.dev>
2025-01-22 15:45:50 +0100

    feat: Add training functionality for WineModel using Wgpu backend
    - Introduced Wgpu and Autodiff backend for enhanced training capabilities.
    - Implemented training setup for WineModel with Adam optimizer.
    - Updated main function to initiate training process and configure device.

--- code/src/main.rs
+++ code/src/main.rs
@@ -1,3 +1,19 @@
+use burn::{
+    backend::{Autodiff, Wgpu},
+    optim::AdamConfig,
+};
+use si_project::model::WineModelConfig;
+use si_project::training::{train, TrainingConfig};
+
 fn main() {
-    println!("Hello, world!");
+    type MyBackend = Wgpu<f32, i32>;
+    type MyAutodiffBackend = Autodiff<MyBackend>;
+
+    let device = burn::backend::wgpu::WgpuDevice::default();
+    let artifact_dir = "./artifacts";
+    train::<MyAutodiffBackend>(
+        artifact_dir,
+        TrainingConfig::new(WineModelConfig::new(10, 512), AdamConfig::new()),
+        device.clone(),
+    );
 }
