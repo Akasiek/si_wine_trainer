151daf4a88a9cb30c9e985e53b42d8c12cc86916
Kamil Pomykała <kamil@kamilpomykala.dev>
2025-01-22 15:45:25 +0100

    feat: Add functionality for loading and batching wine dataset
    - Updated `from_pkl` method to accept a split parameter for dynamic file loading.
    - Introduced `train` and `test` methods for convenience in dataset creation.
    - Implemented `WineBatcher` and `WineBatch` for batching functionality.
    - Enhanced `WineDataset` with `Clone` and custom `Debug` implementations for better usability.

--- code/src/wine.rs
+++ code/src/wine.rs
@@ -1,6 +1,10 @@
+use burn::data::dataloader::batcher::Batcher;
 use burn::data::dataset::{Dataset, InMemDataset};
+use burn::prelude::Backend;
+use burn::tensor::{Int, Tensor, TensorData};
 use serde::{Deserialize, Serialize};
 use std::collections::BTreeMap;
+use std::fmt;
 
 #[derive(Debug, PartialEq, Clone, Serialize, Deserialize)]
 pub struct Wine {
@@ -52,12 +56,12 @@ pub struct WineDataset {
 }
 
 impl WineDataset {
-    pub fn from_pkl() -> Result<Self, std::io::Error> {
+    pub fn from_pkl(split: &str) -> Result<Self, std::io::Error> {
         // Get dataset from data file
-        let x_file = std::fs::File::open("./data/x.pkl").unwrap();
+        let x_file = std::fs::File::open(format!("./data/x_{}.pkl", split))?;
         let x_map: BTreeMap<String, Vec<Vec<f32>>> =
             serde_pickle::from_reader(x_file, Default::default()).unwrap();
-        let y_file = std::fs::File::open("./data/y_t.pkl").unwrap();
+        let y_file = std::fs::File::open(format!("./data/y_t_{}.pkl", split))?;
         let y_map: BTreeMap<String, Vec<i32>> =
             serde_pickle::from_reader(y_file, Default::default()).unwrap();
 
@@ -92,9 +96,33 @@ impl WineDataset {
 
         Ok(dataset)
     }
+
+    pub fn train() -> Self {
+        Self::from_pkl("train").unwrap()
+    }
+
+    pub fn test() -> Self {
+        Self::from_pkl("test").unwrap()
+    }
 }
 
-// Implement the `Dataset` trait which requires `get` and `len`
+impl fmt::Debug for WineDataset {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("WineDataset")
+            .field("dataset", &"InMemDataset<Wine>")
+            .finish()
+    }
+}
+
+impl Clone for WineDataset {
+    fn clone(&self) -> Self {
+        Self {
+            dataset: InMemDataset::new(self.dataset.iter().collect::<Vec<_>>().clone()),
+        }
+    }
+}
+
+/// Implement the `Dataset` trait which requires `get` and `len`
 impl Dataset<Wine> for WineDataset {
     fn get(&self, index: usize) -> Option<Wine> {
         self.dataset.get(index)
@@ -102,5 +130,57 @@ impl Dataset<Wine> for WineDataset {
 
     fn len(&self) -> usize {
         self.dataset.len()
+    }
+}
+
+#[derive(Clone)]
+pub struct WineBatcher<B: Backend> {
+    device: B::Device,
+}
+
+impl<B: Backend> WineBatcher<B> {
+    pub fn new(device: B::Device) -> Self {
+        Self { device }
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct WineBatch<B: Backend> {
+    pub x: Tensor<B, 2>,
+    pub y: Tensor<B, 1, Int>,
+}
+
+impl<B: Backend> Batcher<Wine, WineBatch<B>> for WineBatcher<B> {
+    fn batch(&self, data: Vec<Wine>) -> WineBatch<B> {
+        let x = data
+            .iter()
+            .map(|wine| {
+                Tensor::<B, 2>::from(TensorData::from([[
+                    wine.alcohol,
+                    wine.malic_acid,
+                    wine.ash,
+                    wine.alcalinity_of_ash,
+                    wine.magnesium,
+                    wine.total_phenols,
+                    wine.flavanoids,
+                    wine.nonflavanoid_phenols,
+                    wine.proanthocyanins,
+                    wine.color_intensity,
+                    wine.hue,
+                    wine.od280_od315_of_diluted_wines,
+                    wine.proline,
+                ]]))
+            })
+            .collect::<Vec<_>>();
+
+        let y = data
+            .iter()
+            .map(|wine| Tensor::<B, 1, Int>::from(TensorData::from([wine.class])))
+            .collect::<Vec<_>>();
+
+        let x = Tensor::<B, 2>::cat(x, 0).to_device(&self.device);
+        let y = Tensor::<B, 1, Int>::cat(y, 0).to_device(&self.device);
+
+        WineBatch { x, y }
     }
 }
